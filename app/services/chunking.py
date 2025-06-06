e7c7ddae22c07e37e4e9c10cdcf02ac3923419c5
dandan-levy10 <dandan.levy10@gmail.com>
2025-06-06 15:55:10 +0100

    fix: requirements.txt doesn't contain reference to local filepaths anymore, which was causing error in the CI pipeline

--- app/services/chunking.py
+++ app/services/chunking.py
@@ -1,4 +1,3 @@
-from typing import List
 from langchain.schema import Document
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 import logging
@@ -6,7 +5,7 @@ import logging
 logger = logging.getLogger(__name__)
 
 
-def chunk_documents(documents: List[Document], chunk_size: int, chunk_overlap: int) -> List[Document]:
+def chunk_documents(documents: list[Document], chunk_size: int, chunk_overlap: int) -> list[Document]:
     text_splitter = RecursiveCharacterTextSplitter(
         chunk_size = chunk_size,
         chunk_overlap = chunk_overlap,
